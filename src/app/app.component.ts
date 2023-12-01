334d315f01771eefe6db6d646f0db8a7b776c51c
Bugay Sarikaya <bgysrky@gmail.com>
2023-12-01 17:11:04 +0300

    biringi gun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // template: `
+  // <div>test</div>
+  // <p>test</p>`,
+  styleUrls: ['./app.component.scss'],
+  // encapsulation: ViewEncapsulation.Emulated
+  // encapsulation: ViewEncapsulation.ShadowDom //shadow dom
 })
 export class AppComponent {
   title = 'gib';
