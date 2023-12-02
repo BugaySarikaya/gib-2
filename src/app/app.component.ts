e8eeb4fa1399613be26fa7228aae8a10c6286e91
Bugay Sarikaya <bgysrky@gmail.com>
2023-12-02 11:58:17 +0300

    ikinci-gun-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,5 +11,44 @@ import { Component, ViewEncapsulation } from '@angular/core';
   // encapsulation: ViewEncapsulation.ShadowDom //shadow dom
 })
 export class AppComponent {
-  title = 'gib';
+  title = 'String Interpolation(1-way)';
+  makeRedWithClass: string = 'makeRed';
+  makeRedStyle: string = 'red';
+  imageRef: string =
+    'https://angular.io/assets/images/logos/angular/angular.png';
+  documentLinkRef: string = 'https://angular.io';
+  pText: string = 'Lorem ipsum dolor sit amet.';
+  isButtonDisabled: boolean = true;
+  dynamicValue: string = 'Initial Value';
+  firstName: string = 'John';
+  messageForPipeEx: string = 'Lorem ipsum dolor sit amet.';
+  currentDate: Date = new Date();
+  gibUser = {
+    name: 'John',
+    lastName: 'Doe',
+    age: 30,
+  };
+  randomText: string =
+    'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Exercitationem, minima.';
+
+  getTitle() {
+    return this.title;
+  }
+
+  updateDynamicValue(event: Event) {
+    const inputEvent = event as InputEvent;
+    this.dynamicValue = (inputEvent?.target as HTMLInputElement).value;
+  }
+
+  activateButton() {
+    this.isButtonDisabled = false;
+  }
+
+  firstNameChanged(value: string) {
+    console.log('model changed new value:', value);
+  }
+
+  lastNameChanged(value: Event) {
+    console.log('lastNameChanged:', (value.target as HTMLInputElement).value);
+  }
 }
